868b2bc25ba618452934827e6ac9d99edbdcc9f6
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-09-25 20:50:43 +0300

    Added project

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
-from flask import Flask, render_template, request, session
-from credentials import super_secret
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+import plotly.graph_objs as go
 
+app = dash.Dash()
+application = app.server
 
-app = Flask(__name__)
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
+    'gdp-life-exp-2007.csv')
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server()
